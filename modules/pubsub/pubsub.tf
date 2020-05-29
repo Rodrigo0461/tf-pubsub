fec83af91f92460249bc93f6ea1ced3c839881bf
Rodrigo Pichinual <rodrigo.pichinual@gmail.com>
2020-05-29 16:32:11 -0400

    added vars on file for module

--- modules/pubsub/pubsub.tf
+++ modules/pubsub/pubsub.tf
@@ -1,8 +1,8 @@
 resource "google_pubsub_topic" "topic-a" {
-  name    = "topic-a"
+  name    = "${var.name-topic-a}"
 }
 
 resource "google_pubsub_subscription" "subcription-xa" {
-  name    = "subscription-xa"
+  name    = "${var.name-subscription-xa}"
   topic   = "${google_pubsub_topic.topic-a.name}"
 }
